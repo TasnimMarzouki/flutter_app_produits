a58eb79c7d089556c16f9b893955449407163c27
b300145940 <tonEmail@exemple.com>
2025-11-28 23:37:19 -0500

    Étape 1 : Création du projet Flutter

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'page_liste_produits.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Atelier Produits',
+      home: ListProduits(), 
+    );
+  }
+}
+
+
